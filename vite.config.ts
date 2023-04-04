8182ed67c83547b1e9d660fe293200c0219b2ab1
Khuong Tran <dbcgdhh@gmail.com>
2023-04-04 11:19:47 +0700

    Init

---
+++ vite.config.ts
@@ -0,0 +1,61 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import typescript2 from "rollup-plugin-typescript2";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    }),
+    typescript2({
+      check: false,
+      include: ["src/components/**/*.vue"],
+      tsconfigOverride: {
+        compilerOptions: {
+          outDir: "dist",
+          sourceMap: true,
+          declaration: true,
+          declarationMap: true,
+        },
+      },
+      exclude: ["vite.config.ts"],
+    }),
+  ],
+  build: {
+    cssCodeSplit: true,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: "src/components/main.ts",
+      name: "myLibraryVueTs",
+      formats: ["es", "cjs", "umd"],
+      fileName: format => `global-vue-components-ts.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that should not be bundled
+      // into your library
+      input: {
+        main: resolve(__dirname, "src/components/main.ts"),
+      },
+      external: ["vue"],
+      output: {
+        assetFileNames: assetInfo => {
+          if (assetInfo.name === "main.css")
+            return "global-vue-components-ts.css";
+          return assetInfo.name;
+        },
+        exports: "named",
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+});
