5f37c0537265069c3aa8d1217a8825f1520ebc83
Khuong Tran <dbcgdhh@gmail.com>
2023-04-05 14:25:04 +0700

    on click by vue version

--- src/components/ToggleButton.ts
+++ src/components/ToggleButton.ts
@@ -15,15 +15,38 @@ export const ToggleButton = defineComponent({
       h("div", {}, [
         h(
           "button",
-          {
-            onClick: () => {
-              const vModelEmitEvent = isVue2 ? "input" : "update:modelValue";
-              ctx.emit(vModelEmitEvent, !modelValue.value);
-            },
-          },
+          isVue2
+            ? () =>
+                onClickVue2(() => {
+                  const vModelEmitEvent = isVue2
+                    ? "input"
+                    : "update:modelValue";
+                  ctx.emit(vModelEmitEvent, !modelValue.value);
+                })
+            : () =>
+                onClickVue3(() => {
+                  const vModelEmitEvent = isVue2
+                    ? "input"
+                    : "update:modelValue";
+                  ctx.emit(vModelEmitEvent, !modelValue.value);
+                }),
           "Toggle"
         ),
         h("p", `Selected: ${modelValue.value}`),
       ]);
   },
 });
+
+function onClickVue2(callback: Function) {
+  return {
+    on: {
+      click: () => callback(),
+    },
+  };
+}
+
+function onClickVue3(callback: Function) {
+  return {
+    onClick: () => callback(),
+  };
+}
