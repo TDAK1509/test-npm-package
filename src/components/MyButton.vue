c87082f9d5b573c7b14fcb3e7db9bc8c7d364ac1
Khuong Tran <dbcgdhh@gmail.com>
2023-04-05 08:17:27 +0700

    Update MyButton

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,30 +1,25 @@
-<script setup lang="ts">
-  import { reactive } from 'vue';
-
-  interface Props {
-    primary?: boolean;
-  }
-
-  const props = withDefaults(defineProps<Props>(), {
-    primary: false,
-  });
-
-  const { primary } = reactive(props);
-</script>
-
 <template>
-  <button class="btn" :class="{ primary }">
+  <p>{{  text }}</p>
+  <button class="btn">
     <slot />
   </button>
 </template>
 
+<script setup lang="ts">
+defineProps<{
+  text?: string;
+}>();
+</script>
+
+
+
 <style scoped>
-button {
+.btn {
   padding: .6rem 1.25rem;
   background-color: #747bff;
   border: 0;
 }
-button:hover {
+.btn:hover {
   background-color: #535bf2;
 }
 </style>
